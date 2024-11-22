bc31a68560017a5b9b26f8d1b7ef7a3e8c0e1809
Parker Combs <parker.combs@nih.gov>
2024-11-22 10:02:49 -0600

    Roll back proprietary model support for public version

--- main.py
+++ main.py
@@ -32,14 +32,13 @@ async def query(q: Union[str, None] = None, model: str = "azure-gpt-4o", temp: f
     return {"response": res}
 
 
-@app.get("/queryi/")
-async def query(request: Request, q: Union[str, None] = None, model: str = "azure-gpt-4o", temp: float = 0, max_iterations: int = 10, n_agents: int = 1, summarize: bool = False):
-    if getUserMetadata(req=request.headers) is not None:
-        tpa = tp.ToxPipeAgent(model=model, temp=temp, max_iterations=max_iterations, n_agents=n_agents, summarize=summarize, verbose=False, auth=True)
-        res = tpa.run(q)
-        return {"response": res}
-    else:
-        raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Unauthorized: this endpoint is only available to internal NIEHS users. Please log in with your credentials and try again."
-            )
+#@app.get("/queryi/")
+#async def query(request: Request, q: Union[str, None] = None, model: str = "azure-gpt-4o", temp: float = 0, max_iterations: int = 10, n_agents: int = 1, summarize: bool = False):
+#    if getUserMetadata(req=request.headers) is not None:
+#        tpa = tp.ToxPipeAgent(model=model, temp=temp, max_iterations=max_iterations, n_agents=n_agents, summarize=summarize, verbose=False, auth=True)
+#        res = tpa.run(q)
+#        return {"response": res}
+#    else:
+#        raise HTTPException(
+#                status_code=status.HTTP_401_UNAUTHORIZED,
+#                detail="Unauthorized: this endpoint is only available to internal NIEHS users. Please log in with your credentials and try again."
