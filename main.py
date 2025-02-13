95dcd65ab3274d53b2a5b7c76b9027a400f7c6ba
Parker Combs <parker.combs@nih.gov>
2025-02-13 13:36:04 -0600

    Refactor + persistent storage
    - Adds Postgres connection for persistent agentic memory
    - Adds option to configure max retry number of an agent
    - Overhauls literature search to be more effective
    - Overhauls internal prompt for LLM to allow for more consistent formatting, tool use, and allowance of knowledge from the model's training data
    - Improves tool descriptions, so more tools should be used going forward

--- main.py
+++ main.py
@@ -8,6 +8,31 @@ from langchain.tools.render import render_text_description
 import json
 import datetime
 import uuid
+from langchain_core.load import dumpd, dumps, load, loads
+from langgraph.checkpoint.postgres import PostgresSaver
+from psycopg_pool import ConnectionPool
+import os
+from dotenv import load_dotenv
+load_dotenv('./app/.env')
+
+# Persistent memory
+# Establish Postgres Connection for ToxPipe
+postgres_host = os.environ.get("TOXPIPE_POSTGRES_HOST")
+postgres_port = os.environ.get("TOXPIPE_POSTGRES_PORT")
+postgres_name = os.environ.get("TOXPIPE_POSTGRES_DATABASE")
+postgres_user = os.environ.get("TOXPIPE_POSTGRES_USER")
+postgres_pass = os.environ.get("TOXPIPE_POSTGRES_PASSWORD")
+
+DB_URI = f"postgresql://{postgres_user}:{postgres_pass}@{postgres_host}:{postgres_port}/{postgres_name}"
+connection_kwargs = {
+    "autocommit": True,
+    "prepare_threshold": 0,
+}
+
+checkpointer = None
+pool = ConnectionPool(conninfo=DB_URI, max_size=20, kwargs=connection_kwargs,)
+checkpointer = PostgresSaver(pool)
+checkpointer.setup()
 
 tags_metadata = [
     {
@@ -29,7 +54,7 @@ app = FastAPI(
 
 AGENT_DICT = {}
 ANTHROPIC_MODELS = ['claude-3-5-sonnet', 'claude-3-sonnet', 'claude-3-haiku', 'claude-3-opus'] # haiku and opus work better
-OLLAMA_MODELS = ['llama3-1-70b', 'llama3-1-8b', 'openbiollm-llama3-70b', 'llama2-13b'] # These have trouble with tools
+OLLAMA_MODELS = ['llama3-1-70b', 'llama3-1-8b', 'openbiollm-llama3-70b'] # These have trouble with tools
 OPENAI_MODELS = ['azure-gpt-4o', 'azure-gpt-3.5-turbo', 'azure-gpt-4o-mini', 'azure-gpt-3.5-turbo-16k', 'azure-gpt-4-turbo-20240409', 'azure-gpt-4'] # These all work pretty well
 MISTRALAI_MODELS = ['mistral-large-2', 'mistral-large', 'mistral-7b-instruct', 'mixtral-8x7b-instruct'] # mistral-large-2 and mixtral-8x7b-instruct has issues accessing tools
 GOOGLE_MODELS = ['gemini-1.5-pro'] # TODO - VertexAIException BadRequestError - "Unable to submit request because one or more function parameters didn\'t specify the schema type field. Learn more: https://cloud.google.com/vertex-ai/generative-ai/docs/multimodal/function-calling
@@ -38,9 +63,10 @@ COHERE_MODELS = ['cohere-command-r-plus']
 
 # Must be False for public API, True for Private API. When in doubt, set to False.
 AUTH_MODE = False
+VERBOSE = False
 
 @app.get("/agent/create/", tags=["agent"])
-async def create_agent(request: Request, response: Response, model: str = "azure-gpt-4o", temp: float = 0, max_iterations: int = 10, step_timeout: float = 0, n_threads: int = 1, summarize: bool = False):
+async def create_agent(request: Request, response: Response, model: str = "azure-gpt-4o", temp: float = 0, max_iterations: int = 10, max_retries: int = 10, step_timeout: float = 0, n_threads: int = 1, summarize: bool = False):
 
     # Input validation
     if model not in ANTHROPIC_MODELS and model not in OLLAMA_MODELS and model not in OPENAI_MODELS and model not in MISTRALAI_MODELS and model not in GOOGLE_MODELS and model not in AMAZON_MODELS and model not in COHERE_MODELS:
@@ -53,18 +79,36 @@ async def create_agent(request: Request, response: Response, model: str = "azure
         response.status_code = 400
         return {"response": f"Error: 'n_threads' must be 5 or less."}
 
-    agent_name = uuid.uuid4()
-    tpa = tp.ToxPipeAgent(name=agent_name, model=model, temp=temp, max_iterations=max_iterations, step_timeout=step_timeout, n_agents=n_threads, summarize=summarize, verbose=True, auth=AUTH_MODE)
-    AGENT_DICT[agent_name] = tpa
-    return {"agentid": agent_name, "model": model, "temp": temp, "max_iterations": max_iterations, "step_timeout":step_timeout, "n_threads":n_threads, "summarize":summarize, "date_created":datetime.datetime.now()}
+    agentid = uuid.uuid4()
+    #tpa = tp.ToxPipeAgent(name=agentid, model=model, temp=temp, max_iterations=max_iterations, max_retries=max_retries, step_timeout=step_timeout, n_agents=n_threads, summarize=summarize, verbose=False, auth=AUTH_MODE)
+
+    agent = {"agentid": str(agentid), "model": model, "temp": temp, "max_iterations": max_iterations, "max_retries":max_retries, "step_timeout":step_timeout, "n_threads":n_threads, "summarize":summarize, "date_created":str(datetime.datetime.now())}
+
+    with open(f"./created_agents/{agentid}.json", 'w') as fp:
+        json.dump(agent, fp)
+    return agent
 
 
 @app.get("/agent/query/", tags=["agent"])
 async def query_agent(request: Request, response: Response, agentid: uuid.UUID, q: str):    
-    if agentid not in AGENT_DICT:
+    tpa = None
+    #try:
+    with open(f"./created_agents/{agentid}.json", 'r') as fp:
+        agent = json.load(fp)
+
+        print("==agent==")
+        print(agent)
+
+        tpa = tp.ToxPipeAgent(name=agent["agentid"], model=agent["model"], temp=agent["temp"], max_iterations=agent["max_iterations"], max_retries=agent["max_retries"], step_timeout=agent["step_timeout"], n_agents=agent["n_threads"], summarize=agent["summarize"], verbose=VERBOSE, auth=AUTH_MODE, checkpointer=checkpointer)
+#except Exception as e:
+    #print("Error loading agent from file.")
+    #print(e)
+    #tpa = None
+
+    if tpa is None:
         response.status_code = 400
-        return {"response": f"Error: agent {agentid} not found. Did you initialize the agent?"}
-    tpa = AGENT_DICT[agentid]
+        return {"response": f"Error: agent {agentid} not found or unable to be loaded. Did you initialize the agent?"}
+    
     res = tpa.run(q)
     return {"response": res}
 
@@ -75,9 +119,7 @@ async def view_supported_models(request: Request, response: Response):
 @app.get("/models/tools", tags=["models"])
 async def view_available_tools(request: Request, response: Response):
     tools = render_text_description(tl.make_tools(llm=None, auth=AUTH_MODE))
-
     tools = str(tools).split("\n")
-
     tools_to_return = []
     for tool in tools:
         tmpsplit = tool.split(" - ")
