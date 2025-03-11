ef4899ef8083ada014fd6142e25a375b333ee5f3
Amlan Talukder <amlanaccount@gmail.com>
2025-03-11 12:04:48 -0400

    Added debug facilities temporarily

--- app/agents/tp_tools/ragsearch.py
+++ app/agents/tp_tools/ragsearch.py
@@ -13,7 +13,7 @@ from ...rag import query
 
 def query_rag(q: str, llm: BaseLLM) -> str:
     try:
-        rag_res = query(q, llm=llm)
+        rag_res = query(q, llm=llm)['Response']
         if(len(rag_res) < 1):
             return f"RAG did not find any results for query: {q}."
         else: 
