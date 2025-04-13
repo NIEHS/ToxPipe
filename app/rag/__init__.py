215925120aec0c95b66a2445b6d6b44465d5f69c
Amlan Talukder <amlanaccount@gmail.com>
2025-04-13 12:43:09 -0400

    Minor bug fixed

--- app/rag/__init__.py
+++ app/rag/__init__.py
@@ -66,7 +66,7 @@ def createGraph(llm, use_training_data, relevancy_check):
 
             use_guardrail = False
             if use_guardrail:
-                langgraph.add_node(guardrails)
+                langgraph.add_node(gr.guardrails)
                 langgraph.add_edge(START, 'guardrails')
                 langgraph.add_conditional_edges(
                     'guardrails',
