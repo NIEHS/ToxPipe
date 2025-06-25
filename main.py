d9481d3e0ca438547745894e683dffee4261f89d
Combs <parker.combs@nih.gov>
2025-06-25 12:30:44 -0700

    Ignore cert file

--- main.py
+++ main.py
@@ -4,12 +4,12 @@ import atexit
 from fastapi import FastAPI, Request, Response
 
 # use locally
-from .app.agents import toxpipe as tp
-from .app.agents import tools as tl
+#from .app.agents import toxpipe as tp
+#from .app.agents import tools as tl
 
 # use on posit connect
-#from app.agents import toxpipe as tp
-#from app.agents import tools as tl
+from app.agents import toxpipe as tp
+from app.agents import tools as tl
 
 from langchain.tools.render import render_text_description
 import json
