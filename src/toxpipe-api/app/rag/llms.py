b9cedf698bf402ca3c5cdc71051107a2364865eb
amlantalukder <amlanaccount@gmail.com>
2025-12-26 13:04:42 -0500

    Added additional parameters options for rag api

--- src/toxpipe-api/app/rag/llms.py
+++ src/toxpipe-api/app/rag/llms.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from .utils import Config
 
 # ---------------------------------------------------------------------------
-def getAIModel(model_name: str, temperature: int = 0, is_embedding=False) -> ChatOpenAI | OpenAIEmbeddings:
+def getAIModel(model_name: str, temperature: int = 0, is_embedding=False, **kwargs) -> ChatOpenAI | OpenAIEmbeddings:
     """
     Initializes either an OpenAI Chat LLM object based on the LLM name and temperature
     or an OpenAI embedding model
@@ -24,7 +24,8 @@ def getAIModel(model_name: str, temperature: int = 0, is_embedding=False) -> Cha
             timeout=None,
             max_retries=2,
             seed=1000,
-            http_client=Config.httpx_client
+            http_client=Config.httpx_client,
+            **kwargs
         )
     
     return OpenAIEmbeddings(
